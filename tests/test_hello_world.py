529a4441479f91e359fe1084bd3cf9d6c9d12061
Josh Braseth <jbraseth@uccs.edu>
2025-01-23 19:17:51 -0700

    Added pylint

--- tests/test_hello_world.py
+++ tests/test_hello_world.py
@@ -1,8 +1,10 @@
-from src.hello_world import main
-
 import pytest
 
+from src.hello_world import main
 
+pytestmark = [pytest.mark.hello_world]
+
+
+@pytest.mark.description("main program execution should return should be void")
 def test_main():
     assert main() is None
-
